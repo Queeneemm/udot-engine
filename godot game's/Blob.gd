38b7c257e974aa7a5e6795e41c09e311fee636ad
Queeneemm <kornauhov-vladislav@yandex.ru>
2022-08-19 17:39:16 +0300

    fix game_test first

--- godot game's/Blob.gd
+++ godot game's/Blob.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
-
+var max_hp = 300
+var current_hp
 export var speed = 100
 var velocity = Vector2.ZERO
 var path = []
@@ -9,6 +10,7 @@ var nav = null
 func _ready():
 	yield(owner, "ready")
 	nav = owner.nav
+	current_hp = max_hp
 	$AnimationPlayer.play("Walk")
 	
 func _physics_process(delta):
@@ -25,4 +27,12 @@ func move_to_target():
 		
 func get_target_path(target_pos):
 	path = nav.get_simple_path(global_position, target_pos, false)
+	
+func OnHit(damage):
+	print("mne pizda")
+	current_hp -= damage
+	if current_hp <= 0:
+		queue_free()
+		self.hide()
+
 
