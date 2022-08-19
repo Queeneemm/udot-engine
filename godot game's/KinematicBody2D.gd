f757080423b8622537c48ab9d9b241dc4c6a57e2
Queeneemm <kornauhov-vladislav@yandex.ru>
2022-08-19 18:04:35 +0300

    323

--- godot game's/KinematicBody2D.gd
+++ godot game's/KinematicBody2D.gd
@@ -2,12 +2,11 @@ extends KinematicBody2D
 var hp = 100
 var current_hp
 var direction: Vector2 = Vector2.ZERO
-var damage = 100
+var damage = 25
 export var speed: = 100
+
 func _ready():
 	current_hp = hp
-	pass
-
 func _physics_process(delta):
 	direction = (Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
@@ -21,5 +20,7 @@ func _on_Area2D_body_entered(body):
 	if body.is_in_group("Enemy"):
 		current_hp -= damage
 		if current_hp <= 0:
-			queue_free()
 			self.hide()
+			
+func hp_bar():
+	$TextureProgress.value = current_hp
